0c1987d31e04455d4a06266f51c8c3f00106cda1
Bhargavi Rajamuri <162129694+Bhargavir10@users.noreply.github.com>
2024-10-25 20:06:02 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+ import React from 'react';
+ import './App.css';
+// Move this to the top
+import MyComponent from './components/MyComponent';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, Welcome to My React App!</h1>
+      <p>This is a simple example of a React application.</p>
+    </div>
+  );
+}
+
+export default App;
+
+
